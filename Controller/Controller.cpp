520c255d907bf2a6154927e421e73fe81722e2d4
SAaronB <programmingsean@gmail.com>
2019-01-28 09:37:23 -0700

    Initial commit

---
+++ Controller/Controller.cpp
@@ -0,0 +1,13 @@
+//
+//  Controller.cpp
+//  Vector
+//
+//  Created by Bonilla, Sean on 1/28/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+void Controller :: start(){
+    
+};
---
+++ Controller/Runner.cpp
@@ -0,0 +1,16 @@
+//
+//  Runner.cpp
+//  Vector
+//
+//  Created by Bonilla, Sean on 1/28/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include <stdio.h>
+
+#include "Controller.hpp"
+
+int main(){
+    Controller app;
+    app.start();
+};

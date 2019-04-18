bbaa050d9d6bf8fab4cd65feb5e3e81abfceb7a2
SAaronB <programmingsean@gmail.com>
2019-04-18 09:43:00 -0600

    Started and finished Hashtabel Basic Methods and HashNode

---
+++ Model/Nodes/HashNode.hpp
@@ -0,0 +1,39 @@
+//
+//  HashNode.h
+//  Vector
+//
+//  Created by Bonilla, Sean on 4/18/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef HashNode_hpp
+#define HashNode_hpp
+
+#include "Node.hpp"
+
+template <class Type>
+class HashNode : public Node<Type>{
+private:
+    long key;
+public:
+    HashNode();
+    HashNode(Type data);
+    long getKey() const;
+};
+
+template <class Type>
+HashNode<Type> :: HashNode() {
+    this->key = 0;
+}
+
+template<class Type>
+HashNode<Type> :: HashNode(Type data) : Node<Type>(data){
+    this->key = (long) &data;
+}
+
+template <class Type>
+long HashNode<Type> :: getKey() const {
+    return this->key;
+}
+
+#endif /* HashNode_h */
---
+++ Model/Nodes/Hashtable.hpp
@@ -0,0 +1,143 @@
+//
+//  Hashable.h
+//  Vector
+//
+//  Created by Bonilla, Sean on 4/18/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef Hashtable_hpp
+#define Hashtable_hpp
+
+#include "HashNode.hpp"
+
+template <class Type>
+class Hashtable {
+private:
+    HashNode<Type> ** internalStorage;
+    long size;
+    long capacity;
+    double loadFactor;
+    
+    long getNextPrime();
+    bool isPrime(long current);
+    void resize();
+    long findPosition(HashNode<type>* insertNode);
+    long handleCollision(long index);
+public:
+    Hashtable();
+    ~Hashtable();
+    
+    void insert(Type data);
+    long getSize();
+};
+
+template <class Type>
+void Hashtable<Type> :: insert(Type value) {
+    this->size++;
+    if(((this->size * 1.000) / this->capacity) > this->loadFactor){
+        resize();
+    }
+    
+    HashNode<Type> * temp = new HashNode<Type>(value);
+    long index = findPosition(temp);
+    
+    if(internalStorage[index] == nullptr){
+        internalStorage[index] = temp;
+    }else{
+        long updatedPosition = handleCollision(indx);
+        if(updatedPosition != -1){
+            internalStorage[updatedPosition] = temp;
+        }
+    }
+}
+
+template <class Type>
+void Hashtable<Type> :: resize() {
+    long updatedCapacity = getNextPrime();
+    hashNode<Type> * * tempStorage = new HashNode<Type> * [updatedCapacity];
+    
+    std:: fill_n(tempStorage, updatedCapacity, nullptr);
+    
+    long oldCapacity = this->capacity;
+    this->capacity = updatedCapacity;
+    
+    for(long index = 0; index < oldCapacity; index++){
+        if(internalStorage[index] != nullptr){
+            HashNode<Type> * temp = internalStorage[index];
+            long position = findPosition(temp);
+            if(tempStorage[position] == nullptr){
+                tempStorage[postion] = temp;
+            }else{
+                long updatedPositon = handleCollision(positoin);
+                if(updatedPositon != -1){
+                    tempStorage[updatedPosition] = temp;
+                }
+            }
+        }
+    }
+    internalStorage = tempStorage;
+}
+
+template <class Type>
+long Hashtable<Type> :: handleCollision(long currentPosition) {
+    long shift = 17;
+    
+    for(long position = currentPosition + shift; position != currentPosition; position += shift){
+        if(position >= capacity){
+            position = position % capacity;
+        }
+        
+        if(internalStorage[position] == nullptr){
+            return position;
+        }
+    }
+    return -1;
+}
+
+template <class Type>
+long Hashtable<Type> :: findPosition(HashNode<Type> * insert){
+    long insertPosition = insert->getKey() % this->capacity;
+    return insertPosition;
+}
+
+template <class Type>
+bool Hashtable<Type> :: isPrime(long current) {
+    if(current <= 1){
+        return false;
+    }else if(current == 2 || current == 3){
+        return true;
+    }else if (current % 2 == 0){
+        return false;
+    }else{
+        for(int next = 3; next <= sqrt(current) +1; next += 2){
+            if(current % next == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+template <class Type>
+long Hashtable<Type> :: getNextPrime() {
+    long nextPrime = (this->capacity * 2) + 1;
+    while(!isPrime(nextPrime)){
+        nextPrime += 2;
+    }
+    return nextPrime;
+}
+
+template <class Type>
+Hashtable<Type> :: ~Hashtable() {
+    delete [] internalStorage;
+}
+
+template <class Type>
+Hashtable<Type> :: Hashtable() {
+    capacity = 101;
+    internalStorage = new HashNode<Type> * [capacity];
+    size = 0;
+    loadFactor = .80;
+}
+#endif /* Hashable_h */
